98297d8820f770b4d468438a22681c775f0b8e63
Issarapap <film4777@gmail.com>
2023-04-30 14:52:14 +0700

    axios baseURL

--- configs/axios/axios_config.ts
+++ configs/axios/axios_config.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const axios = Axios.create({
-    baseURL: process.env.BASEURL,
+    baseURL: process.env.BASEURL+"/api",
     headers: { "Content-Type": "application/json" }
 });
 
