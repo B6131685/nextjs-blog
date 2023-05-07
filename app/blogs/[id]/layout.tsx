0c562b1e242a28e88c3cfec2af1461b5f5f51e62
Issarapap <film4777@gmail.com>
2023-05-07 17:31:27 +0700

    remove useRouter inside layout page

--- app/blogs/[id]/layout.tsx
+++ app/blogs/[id]/layout.tsx
@@ -1,10 +1,8 @@
-import { useRouter } from 'next/navigation'
 export default function BlogIDLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter()
   return (
     <>
       {children}
