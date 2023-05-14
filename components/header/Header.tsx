1662e45538f2fbcac2e92032e16d98a7dc263754
Issarapap <film4777@gmail.com>
2023-05-14 19:09:09 +0700

    FF

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { ChangeEvent, KeyboardEvent, useState} from "react";
+import React, { ChangeEvent, KeyboardEvent, useState } from "react";
 import { signIn, useSession } from "next-auth/react";
 import CButton from "../buttons/button";
 import style from "./style.module.scss";
@@ -9,40 +9,39 @@ import Menu from "public/menu.png";
 import { useSideNav, useSideNavDispatch } from "@/providers/sideNav";
 import BlogLogo from "../BlogLogo";
 import UserMenu from "../userMenu/UserMenu";
-import Skeleton from 'react-loading-skeleton'
+import Skeleton from "react-loading-skeleton";
 import InputText from "../input/input";
-import { useRouter } from 'next/navigation';
-import { usePathname } from 'next/navigation';
+import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useQueryBlog } from "@/providers/query";
 import { useQueryClient } from "@tanstack/react-query";
 
 interface Props {
-  hiddenMenuSideNav?: boolean
+  hiddenMenuSideNav?: boolean;
 }
-const Header = ( {hiddenMenuSideNav = false}:Props) => {
+const Header = ({ hiddenMenuSideNav = false }: Props) => {
   const queryBlog = useQueryBlog();
   const pathname = usePathname();
   const router = useRouter();
   const sideNavState = useSideNav();
   const dispatch = useSideNavDispatch();
-  const [search, setSearch] = useState<string>(''); 
+  const [search, setSearch] = useState<string>("");
   const { data: session, status } = useSession();
 
   const queryClient = useQueryClient();
 
   const socialAction = (action: string) => {
-    console.log('socialAction')
-    signIn(action, { redirect: false })
-      .then((callback) => {
-        if (callback?.error) {
-          alert('Invalid credentials!');
-        }
+    console.log("socialAction");
+    signIn(action, { redirect: false }).then((callback) => {
+      if (callback?.error) {
+        alert("Invalid credentials!");
+      }
 
-        if (callback?.ok) {
-          router.push('/blogs')
-        }
-      })
-  } 
+      if (callback?.ok) {
+        router.push("/blogs");
+      }
+    });
+  };
 
   function toggleSideNav() {
     sideNavState.open
@@ -51,22 +50,21 @@ const Header = ( {hiddenMenuSideNav = false}:Props) => {
   }
 
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
-    
-    if (event.key === 'Enter') {
-      if(queryBlog!.dispatch){
-        queryBlog?.dispatch({type:"setTitle", payload:{title: search } })
+    if (event.key === "Enter") {
+      if (queryBlog!.dispatch) {
+        queryBlog?.dispatch({ type: "setTitle", payload: { title: search } });
       }
-      queryClient.removeQueries({ queryKey: ['get-blogs'] });
-      queryClient.refetchQueries({ queryKey: ['get-blogs'] })
-      if( [...(pathname?.split('/') ?? [])].pop() !== 'blogs'){
-        router.push('/blogs')
+      queryClient.removeQueries({ queryKey: ["get-blogs"] });
+      queryClient.refetchQueries({ queryKey: ["get-blogs"] });
+      if ([...(pathname?.split("/") ?? [])].pop() !== "blogs") {
+        router.push("/blogs");
       }
     }
   };
 
   const handleTypeSearch = (event: ChangeEvent<HTMLInputElement>) => {
-    setSearch(event.target.value)
-  } 
+    setSearch(event.target.value);
+  };
 
   return (
     <div className={style.containerQuery}>
@@ -76,8 +74,8 @@ const Header = ( {hiddenMenuSideNav = false}:Props) => {
             hidden={true}
             width={100}
             height={100}
-            className={ `${style.menu} ${hiddenMenuSideNav ? style.hiddle : ''}`}
-            src={ Menu}
+            className={`${style.menu} ${hiddenMenuSideNav ? style.hiddle : ""}`}
+            src={Menu}
             alt="menu.png"
             onClick={() => {
               toggleSideNav();
@@ -86,7 +84,12 @@ const Header = ( {hiddenMenuSideNav = false}:Props) => {
           <Link href="/blogs">
             <BlogLogo />
           </Link>
-          <InputText placeholder="search" value={search} onChange={handleTypeSearch} onKeyDown={handleKeyDown}/>
+          <InputText
+            placeholder="search"
+            value={search}
+            onChange={handleTypeSearch}
+            onKeyDown={handleKeyDown}
+          />
         </section>
         <section className={style.rigth}>
           {(() => {
@@ -95,11 +98,7 @@ const Header = ( {hiddenMenuSideNav = false}:Props) => {
                 return (
                   <>
                     <Link href="/new">
-                      <CButton
-                        variant="blue"
-                      >
-                        Create Blog
-                      </CButton>{" "}
+                      <CButton variant="blue">Create Blog</CButton>{" "}
                     </Link>
                     <div>
                       <UserMenu />
@@ -109,19 +108,15 @@ const Header = ( {hiddenMenuSideNav = false}:Props) => {
               case "unauthenticated":
                 return (
                   <>
-                    <Link href="/login">
-                      <CButton variant="blue" onClick={() => signIn('github')}>
-                        Log in
-                      </CButton>{" "}
-                    </Link>
+                    <CButton variant="blue" onClick={() => signIn("github")}>
+                      Log in
+                    </CButton>{" "}
                   </>
                 );
               case "loading":
-                return (
-                  <Skeleton width={70} height="100%"/> 
-                );
-              default: 
-              return <></>;
+                return <Skeleton width={70} height="100%" />;
+              default:
+                return <></>;
             }
           })()}
         </section>
