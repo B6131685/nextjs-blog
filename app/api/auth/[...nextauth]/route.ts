21420677bea5a6ecc49cb5eada016234aab332e3
Issarapap <film4777@gmail.com>
2023-05-14 21:32:08 +0700

    assertion

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -7,8 +7,8 @@ const handler = NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
   ],
   callbacks: {
