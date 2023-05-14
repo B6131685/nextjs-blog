f51c98057097316983e1830e9919a4d7c5a70978
Issarapap <film4777@gmail.com>
2023-05-14 21:45:34 +0700

    remove

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,6 @@
 import { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { withAuth } from "next-auth/middleware";
-
-
-export default withAuth({
-  pages: {
-    signIn: "/",
-  },
-});
 
 
 export async function middleware(request: NextRequest) {
