b2743aefd1018a9559a646af27ebfd2edde044d3
Issarapap <film4777@gmail.com>
2024-02-04 12:14:40 +0700

    set axios header tag request

--- services/tag/index.ts
+++ services/tag/index.ts
@@ -5,7 +5,11 @@ import { ITage } from "./interface";
 export function useGetTags():UseQueryResult<ITage[]> {
     return useQuery(['tags'], async () => {
       const res = await axios.get(`/tag`,{ headers: {
-        cache: 'no-store'
+        "cache": 'no-store',
+        "Access-Control-Allow-Credentials":"true",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods":  "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+        "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
       }});   
   
       if (res.status >= 200 && res.status < 300)  {
