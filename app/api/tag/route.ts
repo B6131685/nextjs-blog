c5fe788d3fcbb7c4c7dd734c2ba40a6fdffe5fa9
Issarapap <film4777@gmail.com>
2024-02-04 12:56:30 +0700

    ** *

--- app/api/tag/route.ts
+++ app/api/tag/route.ts
@@ -9,7 +9,7 @@ export async function GET() {
     const tageArr = await tagsCollection.find({}).toArray();
     return NextResponse.json(tageArr, { status: 200 , headers:[
       ["Access-Control-Allow-Credentials","true"],
-      ["Access-Control-Allow-Origin","**"],
+      ["Access-Control-Allow-Origin","*"],
       ["Access-Control-Allow-Methods","GET,OPTIONS,PATCH,DELETE,POST,PUT"],
       ["Access-Control-Allow-Headers","X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"]
     ]});
