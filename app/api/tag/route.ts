32269cd1c5b42f2d03097d64e631e77bd90bee26
Issarapap <film4777@gmail.com>
2024-02-04 13:13:26 +0700

    export option

--- app/api/tag/route.ts
+++ app/api/tag/route.ts
@@ -7,16 +7,23 @@ export async function GET() {
   try {
     const tagsCollection = await TagCollection();
     const tageArr = await tagsCollection.find({}).toArray();
-    return NextResponse.json(tageArr, { status: 200 , headers:[
-      ["Access-Control-Allow-Credentials","true"],
-      ["Access-Control-Allow-Origin","*"],
-      ["Access-Control-Allow-Methods","GET,OPTIONS,PATCH,DELETE,POST,PUT"],
-      ["Access-Control-Allow-Headers","X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"]
-    ]});
+    return NextResponse.json(tageArr, { status: 200});
   } catch (error) {
     if (error instanceof Error) {
       return NextResponse.json({message: error.message}, { status: 500 });
     }
     return NextResponse.json({error},{status:500})
   }
+}
+
+export const OPTIONS = async () => {
+  return new NextResponse('', {
+    status: 200,
+    headers:[
+      ["Access-Control-Allow-Credentials","true"],
+      ["Access-Control-Allow-Origin","*"],
+      ["Access-Control-Allow-Methods","GET,OPTIONS,PATCH,DELETE,POST,PUT"],
+      ["Access-Control-Allow-Headers","X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"]
+    ]
+  })
 }
